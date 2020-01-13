11eb59a52569b5afa787d5b6d7edcee156de8687
saviojoseph <49006729+saviojoseph@users.noreply.github.com>
2020-01-13 15:02:24 -0500

    Add files via upload

---
+++ Question - 509 Fibonacci Number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fib(int N) {
+      int[] arr = new int[N+1];
+      
+    
+        if(N == 0){
+            return 0;
+        }else if (N == 1){
+            return 1;
+        }
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        for(int i = 2; i < arr.length; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+            
+        }
+        
+      return arr[N];
+    }
+}
